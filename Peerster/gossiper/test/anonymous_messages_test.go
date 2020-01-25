946e15ea053cc683fc2d96a769269c48aa6d3118
Aleksandar Hrusanov <aleksandar.hrusanov@epfl.ch>
2020-01-25 22:15:45 +0100

    Cluster info not spreading yet

--- Peerster/gossiper/test/anonymous_messages_test.go
+++ Peerster/gossiper/test/anonymous_messages_test.go
@@ -1,12 +1,12 @@
 package test
 
 import (
-	"fmt"
+	"testing"
+	"time"
+
 	"github.com/JohanLanzrein/Peerster/gossiper"
 	"github.com/stretchr/testify/assert"
 	"go.dedis.ch/onet/log"
-	"testing"
-	"time"
 )
 
 func TestAnonymousMessaging(t *testing.T) {
@@ -21,7 +21,7 @@ func TestAnonymousMessaging(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	g3, err := gossiper.NewGossiper("C", "8082", "127.0.0.1:5002", "127.0.0.1:5001", false, 10, "8002", 10, 3, 5, false, 10, false)
+	g3, err := gossiper.NewGossiper("C", "8082", "127.0.0.1:5002", "127.0.0.1:5000", false, 10, "8002", 10, 3, 5, false, 10, false)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -38,32 +38,48 @@ func TestAnonymousMessaging(t *testing.T) {
 	for g1.FindPath(g3.Name) == "" {
 		log.Warn("Path not established yet. Waiting 5 more seconds")
 		<-time.After((5 * time.Second))
-
 	}
+
+	for g1.FindPath(g2.Name) == "" {
+		log.Warn("Path not established yet. Waiting 5 more seconds")
+		<-time.After((5 * time.Second))
+	}
+
+	for g2.FindPath(g3.Name) == "" {
+		log.Warn("Path not established yet. Waiting 5 more seconds")
+		<-time.After((5 * time.Second))
+	}
+
 	//G1 joins a cluster...
 	g1.InitCluster()
 	//G2 asks to join it..
 	<-time.After(1 * time.Second)
+	g2.RequestJoining(g1.Name, *g1.Cluster.ClusterID)
 	g3.RequestJoining(g1.Name, *g1.Cluster.ClusterID)
-	<-time.After(5 * time.Second)
+	<-time.After(10 * time.Second)
 	c3 := g3.Cluster
+	c2 := g2.Cluster
 	c1 := g1.Cluster
 	assert.Equal(t, c1.ClusterID, c3.ClusterID)
+	assert.Equal(t, c1.ClusterID, c2.ClusterID)
 	assert.Equal(t, c1.MasterKey, c3.MasterKey)
+	assert.Equal(t, c1.MasterKey, c2.MasterKey)
 	assert.Equal(t, c1.Members, c3.Members)
+	assert.Equal(t, c1.Members, c2.Members)
 	assert.Equal(t, c1.PublicKeys, c3.PublicKeys)
-	text := "Hello from g3!"
-	_, _ = g1.ReplyToClient()
-
-	g3.SendBroadcast(text, false )
-
-	<-time.After(3 * time.Second)
-	b1, err := g1.ReplyToClient()
-	if err != nil {
-		log.ErrFatal(err, "Could not get the buffer")
-	}
-
-	exp := fmt.Sprint("Broadcast origin ", g3.Name, " contents ", text, "\n")
-	assert.Equal(t, string(b1), exp)
+	assert.Equal(t, c1.PublicKeys, c2.PublicKeys)
+	// text := "Hello from g3!"
+	// _, _ = g1.ReplyToClient()
+	//
+	// g3.SendBroadcast(text, false)
+	//
+	// <-time.After(3 * time.Second)
+	// b1, err := g1.ReplyToClient()
+	// if err != nil {
+	// 	log.ErrFatal(err, "Could not get the buffer")
+	// }
+	//
+	// exp := fmt.Sprint("Broadcast origin ", g3.Name, " contents ", text, "\n")
+	// assert.Equal(t, string(b1), exp)
 
 }
